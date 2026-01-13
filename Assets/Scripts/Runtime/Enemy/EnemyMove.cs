5cc51d2f59d224614877adc1d9be01acdbd48443
Evelien <edewals2009@gmail.com>
2026-01-13 16:55:46 +0100

    Added Enemy movement and player tracking

---
+++ Assets/Scripts/Runtime/Enemy/EnemyMove.cs
@@ -0,0 +1,118 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField] List<Transform> Locations;
+    [Tooltip("m/s")]
+    [SerializeField] private float speed = 2;
+    [Tooltip("Angle in degrees")]  
+    [SerializeField] private float fieldOfView;
+    [Tooltip("Length of field of view")]
+    [SerializeField] private float viewDistance;
+    [Tooltip("enemy doesn't go closer to player then this")]
+    [SerializeField] private float minDistanceToPlayer;
+    [Tooltip("m/s^2")]
+    [SerializeField] private float gravityStrength = 2f;
+    [Tooltip("m/s")]
+    [SerializeField] private float maxFallSpeed = 10f;
+
+    [SerializeField] private Transform eyeTransform;
+    [SerializeField] private Transform Target;
+
+    [SerializeField] private LayerMask PlayerLayer;
+
+    private int currentLocation;
+    private int nextLocation;
+    private float acceptanceRadius = 0.7f;
+    private bool isFollowing = false;
+    private float verticalVelocity;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+
+    void FixedUpdate()
+    {
+        CheckLineOfSight();
+
+        if (isFollowing) 
+        {
+            float distance = Vector3.Distance(transform.position, Target.position);
+            transform.LookAt(new Vector3(Target.position.x, transform.position.y, Target.position.z));
+            HandleGravity();
+            controller.Move(Time.deltaTime * new Vector3(0, verticalVelocity, 0));
+            if (distance > minDistanceToPlayer)
+            {
+                Vector3 direction = (Target.position - transform.position);
+                direction.y = 0;
+                controller.Move(direction.normalized * Time.deltaTime * speed);
+            }
+        }
+        else
+        {
+            if (Locations.Count == 0 || Locations == null)
+            {
+                return;
+            }
+            if (currentLocation < Locations.Count)
+            {
+                FollowInstuctions();
+            }
+            else
+            {
+                currentLocation = 0;
+            }
+        }
+    }
+
+    void FollowInstuctions()
+    {
+        if (Vector3.Distance(transform.position, Locations[nextLocation].position) < acceptanceRadius)
+        {
+            currentLocation = nextLocation;
+            nextLocation = (currentLocation + 1) % Locations.Count;
+        }
+        Vector3 lookDirection = Locations[nextLocation].position - transform.position;
+        lookDirection.y = 0; // never look down or up
+        lookDirection.Normalize();
+        Vector3 upDirection = Vector3.up;
+
+        transform.rotation = Quaternion.LookRotation(lookDirection, upDirection);
+        Vector3 moveDirection = lookDirection * Time.deltaTime * speed;
+        if ((moveDirection).magnitude > (Locations[nextLocation].position - transform.position).magnitude)
+        {
+            moveDirection = Locations[nextLocation].position - transform.position;
+        }
+
+        controller.Move(moveDirection);
+    }
+    void CheckLineOfSight()
+    {
+        if (ConePhysics.ConeCast(out RaycastHit hit, eyeTransform.position, eyeTransform.forward, fieldOfView, 3, 0.5f, viewDistance, PlayerLayer, false, 0.1f, QueryTriggerInteraction.Ignore, true, Color.cyan))
+        {
+            isFollowing = true;
+        }
+
+        else { isFollowing = false; }
+    }
+    public void HandleGravity()
+    {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -0.1f; // keep grounded
+        }
+        else
+        {
+            if (verticalVelocity < maxFallSpeed)
+            {
+                verticalVelocity -= gravityStrength * Time.fixedDeltaTime;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Enemy/EnemyVision.cs
@@ -0,0 +1,115 @@
+﻿using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class EnemyVision : MonoBehaviour
+{
+    public Transform player;
+    public float visionRange = 15f;
+    public float detectionSpeed = 1f;      // how fast the bar fills
+    public float decaySpeed = 0.5f;        // how fast the bar decreases
+    public float maxDetection = 5f;        // max counter
+
+    private bool playerInCollider = false;
+    private bool dialogueTriggered = false;
+    public float detectionCounter = 0f;
+
+
+
+    void Update()
+    {
+        if (CanSeePlayer())
+        {
+            detectionCounter += detectionSpeed * Time.deltaTime; //detection goes up over time when the player is in sight
+        }
+        else
+        {
+            detectionCounter -= decaySpeed * Time.deltaTime;
+        }
+
+        detectionCounter = Mathf.Clamp(detectionCounter, 0, maxDetection); //detectionCounter should be between 0 and maxDetection
+
+        if (detectionCounter >= maxDetection && dialogueTriggered==false)
+        {
+            dialogueTriggered = true;  
+            detectionCounter = 0f;    // reset
+        }
+        else
+        {
+            dialogueTriggered = false; //reset
+        }
+
+    }
+
+    bool CanSeePlayer()
+    {
+        if(playerInCollider == true)
+        {
+            Vector3 lookDirection = player.position - transform.position; // difference vector (see paint)
+            lookDirection.y = 0; // never look down or up (locking rotation)
+            Vector3 upDirection = Vector3.up;
+
+            transform.rotation =
+                Quaternion.LookRotation(lookDirection, upDirection); 
+
+
+            // Raycast
+            if (Physics.Raycast(transform.position,
+                                transform.forward,
+                                out RaycastHit hit,
+                                visionRange))
+
+            {
+                if (hit.collider.CompareTag("PlayerRayCollider")) 
+                {
+                    Debug.DrawLine(transform.position, hit.point, Color.red);
+
+                    //Debug.Log("PlayerHit");
+                    return true;
+                }
+                else
+                {
+                    Debug.DrawLine(transform.position, hit.point, Color.green);
+                }
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.forward * visionRange, Color.blue);
+
+            }
+        }
+        return false;
+    }
+
+    void OnTriggerEnter(Collider other)  //make sure enemy only looks when player is in a certain range
+    {
+        
+        if (other.CompareTag("PlayerRayCollider"))
+        {
+            playerInCollider = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("PlayerRayCollider") && detectionCounter < 0)
+        {
+            playerInCollider = false;
+            transform.rotation = Quaternion.Euler(0, 0, 0); //set rotation back to origin
+        }
+        else if (other.CompareTag("PlayerRayCollider") && detectionCounter > 0)
+        {
+            playerInCollider = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) //stop looking when player out of range
+    {
+        if (other.CompareTag("PlayerRayCollider"))
+        {
+            playerInCollider = false;
+        }
+    }
+}
+
