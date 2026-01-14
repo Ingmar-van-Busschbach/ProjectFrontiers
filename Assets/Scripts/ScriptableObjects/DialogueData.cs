e76cb1223189cba91f485b72ff549333814d38c8
Ingmar van Busschbach <ingmarmatthis@gmail.com>
2026-01-14 10:15:07 +0100

    Cleaned up assets and added dialogue system

---
+++ Assets/Scripts/ScriptableObjects/DialogueData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/DialogueData", order = 1)]
+public class DialogueData : ScriptableObject
+{
+    public StructLibrary.Struct_DialogueEntry[] dialogue;
+}
