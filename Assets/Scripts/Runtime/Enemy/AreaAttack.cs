06b24f0a8166ade01b4d9688da10ed6349ae816a
Evelien <edewals2009@gmail.com>
2026-01-21 14:13:51 +0100

    Made the area attack damage deal damage per second

--- Assets/Scripts/Runtime/Enemy/AreaAttack.cs
+++ Assets/Scripts/Runtime/Enemy/AreaAttack.cs
@@ -1,7 +1,6 @@
-using System;
-using Unity.Collections;
+using System.Collections;
 using UnityEngine;
-using static UnityEngine.Analytics.IAnalytic;
+
 
 public class AreaAttack : MonoBehaviour
 {
@@ -9,10 +8,13 @@ public class AreaAttack : MonoBehaviour
     [SerializeField] private float timeToDamage;
     [SerializeField] private float damageRadius;
     [SerializeField] private float damagePerSecond;
-    [SerializeField] private float damageDuration;
+    [SerializeField] private float damageDuration = 3;
+    [Tooltip("The amount of times damage/second is divided (the higher the smoother)")]
+    [SerializeField] private float ticksPerSecond = 10;
     
     [SerializeField] LayerMask layerToHit;
     [SerializeField] EnumLibrary.EDamageType damageType;
+    private bool startedDamage;
 
     private float timeElapsed;
 
@@ -24,14 +26,31 @@ public class AreaAttack : MonoBehaviour
         {
             return;
         }
-            
-        Collider[] damageCollider = Physics.OverlapSphere(transform.position, damageRadius, layerToHit, QueryTriggerInteraction.Ignore);
-        foreach (Collider col in damageCollider)
+        if (!startedDamage)
         {
-            if (col.gameObject.TryGetComponent<IDamageAble>(out IDamageAble target))
+            startedDamage = true;
+            StartCoroutine(DealDamage());
+        }
+
+    }
+
+    private IEnumerator DealDamage()
+    {
+        
+        float currentDuration = 0;
+        while (currentDuration <= damageDuration)
+        {
+            currentDuration += 1 / ticksPerSecond;
+            Collider[] damageCollider = Physics.OverlapSphere(transform.position, damageRadius, layerToHit, QueryTriggerInteraction.Ignore);
+            foreach (Collider col in damageCollider)
             {
-                target.ApplyDamage(damagePerSecond, damageType, new RaycastHit());
+                if (col.gameObject.TryGetComponent<IDamageAble>(out IDamageAble target))
+                {
+                    target.ApplyDamage(damagePerSecond / ticksPerSecond, damageType, new RaycastHit());
+
+                }
             }
+            yield return new WaitForSeconds(1 / ticksPerSecond);
         }
 
         Destroy(gameObject);
