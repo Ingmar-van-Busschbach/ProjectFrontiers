04c5fcd4a16b7fe941646e837a0f613b537b6734
Ingmar van Busschbach <ingmarmatthis@gmail.com>
2026-01-26 10:36:28 +0100

    Moved audio files and created a volume settings prefab

---
+++ Assets/Scripts/Runtime/UI/AudioSlider.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSlider : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public string volumeParameterName;
+
+    public void SetVolume(float volume)
+    {
+        mixer.SetFloat(volumeParameterName, Mathf.Log10(volume + 0.0001f) * 20);
+    }
+}
